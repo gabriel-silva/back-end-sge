409847b15b7aced780a0bd2ffae3a2d78eaa543a
Gabriel Neves <gabriel-silva_2011@hotmail.com>
2020-03-19 00:08:57 -0300

    inicio de projeto

---
+++ src/main/java/com/backend/sge/BackEndSgeApplication.java
@@ -0,0 +1,13 @@
+package com.backend.sge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndSgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndSgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/sge/BackEndSgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.sge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndSgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
