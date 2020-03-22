a5dd9c39fee411245de5626f437232d763b1806e
Gabriel Neves <gabriel-silva_2011@hotmail.com>
2020-03-21 23:26:24 -0300

    apagando coisas desnecessárias do stock no projeto

--- src/main/java/com/backend/sge/repository/StockRepository.java
+++
@@ -1,7 +0,0 @@
-package com.backend.sge.repository;
-
-import com.backend.sge.model.Stock;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface StockRepository extends PagingAndSortingRepository<Stock, Long> {
-}
