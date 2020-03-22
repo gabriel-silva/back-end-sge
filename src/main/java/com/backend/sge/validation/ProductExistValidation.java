50be5c24aa6bf88ef4f8f8d069c4f3389a32cbc6
Gabriel Neves <gabriel-silva_2011@hotmail.com>
2020-03-21 23:13:12 -0300

    adicionando ProductExistValidation

---
+++ src/main/java/com/backend/sge/validation/ProductExistValidation.java
@@ -0,0 +1,27 @@
+package com.backend.sge.validation;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class ProductExistValidation {
+
+    @NotNull(message = "código do produto é obrigatório")
+    @PositiveOrZero(message = "código do produto não pode ser negativo")
+    private Integer idProduct;
+
+    @NotNull(message = "quantidade é obrigatório")
+    private Integer qtd;
+
+    @NotNull(message = "valor unitário é obrigatório")
+    private Double unitaryValue;
+
+}
